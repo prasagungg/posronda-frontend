fd805829ec2cff3d3d279caf11832c5771206587
prasagungg <agungprasetio878@gmail.com>
2022-04-07 16:17:05 +0700

    header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,25 +14,59 @@ function Navbar() {
   };
 
   return (
-    <nav className="drop-shadow-lg w-100 h-20 bg-primary-gray">
-      <div className="container mx-auto flex justify-between px-2 py-2 h-full">
-        <div className="my-auto">
+    <nav className="navbar bg-primary-gray">
+      <div className="container mx-auto px-2 py-2 h-full flex justify-between">
+        <div className="w-1/4">
           <Link className="font-grape text-2xl font-bold" to="/">
             Pos Ronda
           </Link>
         </div>
-        <div className="my-auto flex justify-between">
-          {pathMatchRoute("/") ? (
-            <AiFillHome
-              onClick={() => navigate("/")}
-              className="w-6 h-6 hover:cursor-pointer"
+        <div className="w-2/4 flex justify-between">
+          <div class="form-control">
+            <input
+              type="text"
+              placeholder="Search"
+              class="input input-bordered"
             />
-          ) : (
-            <AiOutlineHome
-              onClick={() => navigate("/")}
-              className="w-6 h-6 hover:cursor-pointer"
-            />
-          )}
+          </div>
+          <button class="btn btn-ghost btn-circle">
+            {pathMatchRoute("/") ? (
+              <AiFillHome
+                onClick={() => navigate("/")}
+                className="w-6 h-6 hover:cursor-pointer"
+              />
+            ) : (
+              <AiOutlineHome
+                onClick={() => navigate("/")}
+                className="w-6 h-6 hover:cursor-pointer"
+              />
+            )}
+          </button>
+
+          <div class="dropdown dropdown-end">
+            <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+              <div class="w-10 rounded-full">
+                <img
+                  src="https://cdn.discordapp.com/attachments/785540914323914785/961555377063882792/unknown.png"
+                  alt="logo-profile"
+                />
+              </div>
+            </label>
+            <ul
+              tabindex="0"
+              class="mt-5 p-2 shadow-lg menu menu-compact dropdown-content bg-primary-gray rounded-box w-52"
+            >
+              <li>
+                <Link to="/profile">Profile</Link>
+              </li>
+              <li>
+                <Link to="/settings">Settings</Link>
+              </li>
+              <li>
+                <Link to="/logout">Logout</Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
